3343229fff0675235ac8bda843363f3ccd1bd068
Renan Gomes <renango2015@gmail.com>
2024-02-16 01:27:54 -0300

    refactor: extract filterFeedback function to separate data-access file

--- src/helpers/queryAndSortProductFeedbacks.ts
+++ src/helpers/queryAndSortProductFeedbacks.ts
@@ -1,8 +1,11 @@
-import { prisma } from '@/lib/prisma';
+import { filterFeedback } from '@/data-access/product-feedback';
 
 const filterOption = ['ALL', 'UI', 'UX', 'ENHANCEMENT', 'BUG', 'FEATURE'] as const;
 const orderByOptions = ['upvotes', 'comments'] as const;
 const orderOptions = ['asc', 'desc'] as const;
+
+export type OrderByFilter = { upvotes: 'asc' | 'desc' } | { comments: { _count: 'asc' | 'desc' } };
+export type CategoryFilter = Exclude<SearchParamsFilter['filter'], 'ALL'> | undefined;
 
 export type SearchParamsFilter = {
   filter: (typeof filterOption)[number];
@@ -11,17 +14,13 @@ export type SearchParamsFilter = {
 };
 
 export const queryAndSortProductFeedbacks = async ({ filter, orderBy, order }: SearchParamsFilter) => {
-  const categoryFilter = filterOption.includes(filter) && filter !== 'ALL' ? filter : {};
+  const categoryFilter = filterOption.includes(filter) && filter !== 'ALL' ? filter : undefined;
   const orderFilter = orderOptions.includes(order) ? order : 'desc';
   const isOrderByValid = orderByOptions.includes(orderBy);
 
   const orderByComments = isOrderByValid && orderBy === 'comments' && { comments: { _count: orderFilter } };
   const orderByUpvotes = isOrderByValid && orderBy === 'upvotes' && { upvotes: orderFilter };
-  const orderByFilter = orderByUpvotes || orderByComments || {};
+  const orderByFilter = orderByUpvotes || orderByComments || { upvotes: 'desc' };
 
-  return await prisma.productFeedback.findMany({
-    orderBy: orderByFilter,
-    where: { category: categoryFilter },
-    include: { _count: { select: { comments: true } } },
-  });
+  return await filterFeedback(orderByFilter, categoryFilter);
 };
