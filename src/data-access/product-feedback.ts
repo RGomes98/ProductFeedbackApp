2fbc10ee1b4c51d90369097837cc0ee52927a673
Renan Gomes <renango2015@gmail.com>
2024-02-16 02:14:44 -0300

    feat(data-access): add filterFeedback function

--- src/data-access/product-feedback.ts
+++ src/data-access/product-feedback.ts
@@ -1,3 +1,4 @@
+import type { CategoryFilter, OrderByFilter } from '@/helpers/queryAndSortProductFeedbacks';
 import type { Feedback } from '@/lib/schemas/feedback-schema';
 import { prisma } from '@/lib/prisma';
 
@@ -11,6 +12,14 @@ export const incrementFeedbackUpvote = async (feedbackId: number) => {
 
 export const decrementFeedbackUpvote = async (feedbackId: number) => {
   await prisma.productFeedback.update({ where: { id: feedbackId }, data: { upvotes: { decrement: 1 } } });
+};
+
+export const filterFeedback = async (orderByFilter: OrderByFilter, categoryFilter: CategoryFilter) => {
+  return await prisma.productFeedback.findMany({
+    orderBy: orderByFilter,
+    where: { category: categoryFilter },
+    include: { _count: { select: { comments: true } } },
+  });
 };
 
 export const getFeedback = async (feedbackId: number) => {
