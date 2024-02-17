3e42a05e177bdcaf2fe1577a8f7b22e1b1fe24c5
Renan Gomes <renango2015@gmail.com>
2024-02-17 06:26:39 -0300

    refactor: remove redundant conditional logic for datasource URL configuration in PrismaClient initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,3 @@
 import { PrismaClient } from '@prisma/client';
-import { env } from '@/env';
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: { url: env.NODE_ENV === 'development' ? env.POSTGRES_URL_NO_SSL : env.POSTGRES_PRISMA_URL },
-  },
-});
+export const prisma = new PrismaClient();
