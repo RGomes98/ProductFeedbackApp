0acb2fb85bb0345d6fcfe10aa7c3a44305d30e08
Renan Gomes <renango2015@gmail.com>
2024-02-17 05:51:21 -0300

    chore: update environment variables

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,6 +3,6 @@ import { env } from '@/env';
 
 export const prisma = new PrismaClient({
   datasources: {
-    db: { url: env.NODE_ENV === 'development' ? env.POSTGRES_PRISMA_URL_NO_SSL : env.POSTGRES_PRISMA_URL },
+    db: { url: env.NODE_ENV === 'development' ? env.POSTGRES_URL_NO_SSL : env.POSTGRES_PRISMA_URL },
   },
 });
