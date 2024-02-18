f97c90ef0541236fdc1cfcd17b6d5fdd3fe1c873
Renan Gomes <renango2015@gmail.com>
2024-02-18 14:43:29 -0300

    feat: add SVG icons as React components

---
+++ src/components/Icons/ArrowDownIcon.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from 'react';
+
+export const ArrowDownIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={10} height={7} xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path d='M1 1l4 4 4-4' stroke='#4661E6' strokeWidth={2} fill='none' fillRule='evenodd' />
+  </svg>
+);
---
+++ src/components/Icons/ArrowUpIcon.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from 'react';
+
+export const ArrowUpIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={10} height={7} xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path d='M1 6l4-4 4 4' stroke='#4661E6' strokeWidth={2} fill='none' fillRule='evenodd' />
+  </svg>
+);
