daf2e1ee50007c0445bf83ebf9eac027ce897ef6
Renan Gomes <renango2015@gmail.com>
2024-02-28 12:07:31 -0300

    feat(prisma): add username to User table

--- src/auth.ts
+++ src/auth.ts
@@ -16,8 +16,45 @@ export const {
   secret: env.NEXT_AUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
-    Google({ clientId: env.GOOGLE_CLIENT_ID, clientSecret: env.GOOGLE_CLIENT_SECRET }),
-    Github({ clientId: env.GITHUB_CLIENT_ID, clientSecret: env.GITHUB_CLIENT_SECRET }),
-    Twitter({ clientId: env.TWITTER_CLIENT_ID, clientSecret: env.TWITTER_CLIENT_SECRET }),
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      profile: ({ sub, name, picture, email }) => {
+        return {
+          id: sub,
+          name: name,
+          email: email,
+          image: picture,
+          username: email.split('@')[0].toLowerCase(),
+        };
+      },
+    }),
+
+    Github({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+      profile: ({ id, name, email, login, avatar_url }) => {
+        return {
+          name: name,
+          email: email,
+          id: String(id),
+          image: avatar_url,
+          username: login.toLowerCase(),
+        };
+      },
+    }),
+
+    Twitter({
+      clientId: env.TWITTER_CLIENT_ID,
+      clientSecret: env.TWITTER_CLIENT_SECRET,
+      profile: ({ data: { id, name, username, profile_image_url } }) => {
+        return {
+          id: id,
+          name: name,
+          image: profile_image_url,
+          username: username.toLowerCase(),
+        };
+      },
+    }),
   ],
 });
