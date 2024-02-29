14c0c6a14d5cfe3af702c6d761bad79c1cfa2e04
Renan Gomes <renango2015@gmail.com>
2024-02-29 05:31:43 -0300

    feat: implement path revalidation and server-side redirect in server actions

--- src/server-actions/create-feedback-comment-action.ts
+++ src/server-actions/create-feedback-comment-action.ts
@@ -1,27 +1,24 @@
 'use server';
 
+import { invalidFieldsErrorResponse, resolveHTTPResponse } from '@/utils/serverActionsResponses';
 import { feedbackCommentSchema } from '@/lib/schemas/feedback-comment-schema';
 import type { FormState } from '@/hooks/useServerActionFormState';
 import { createFeedbackComment } from '@/data-access/comment';
 import { assertIsError } from '@/utils/assertIsError';
 import { getFeedback } from '@/data-access/feedback';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { auth } from '@/auth';
-
-import {
-  authenticationErrorResponse,
-  invalidFieldsErrorResponse,
-  resolveHTTPResponse,
-} from '@/utils/serverActionsResponses';
 
 export const createFeedbackCommentAction = async (
   formState: FormState,
   formData: FormData
 ): Promise<FormState> => {
   const feedbackComment = feedbackCommentSchema.safeParse(Object.fromEntries(formData));
-  const fields = new Set(['content', 'feedbackId']);
+  const fields = new Set(['path', 'content', 'feedbackId']);
   const userId = (await auth())?.user?.id;
 
-  if (!userId) return authenticationErrorResponse(fields);
+  if (!userId) redirect('/login');
 
   if (!feedbackComment.success) return invalidFieldsErrorResponse(formData, feedbackComment.error, fields);
 
@@ -29,6 +26,7 @@ export const createFeedbackCommentAction = async (
     const feedback = await getFeedback(feedbackComment.data.feedbackId);
     createFeedbackComment(userId, { ...feedbackComment.data, feedbackId: feedback.id });
 
+    revalidatePath(feedbackComment.data.path);
     return resolveHTTPResponse('OK', 'comment successfully created', fields);
   } catch (error) {
     assertIsError(error);
--- src/server-actions/create-feedback-reply-action.ts
+++ src/server-actions/create-feedback-reply-action.ts
@@ -1,27 +1,24 @@
 'use server';
 
+import { invalidFieldsErrorResponse, resolveHTTPResponse } from '@/utils/serverActionsResponses';
 import { feedbackReplySchema } from '@/lib/schemas/feedback-reply-schema';
 import { FormState } from '@/hooks/useServerActionFormState';
 import { getFeedbackComment } from '@/data-access/comment';
 import { createFeedbackReply } from '@/data-access/reply';
 import { assertIsError } from '@/utils/assertIsError';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { auth } from '@/auth';
-
-import {
-  authenticationErrorResponse,
-  invalidFieldsErrorResponse,
-  resolveHTTPResponse,
-} from '@/utils/serverActionsResponses';
 
 export const createFeedbackReplyAction = async (
   formState: FormState,
   formData: FormData
 ): Promise<FormState> => {
   const feedbackReply = feedbackReplySchema.safeParse(Object.fromEntries(formData));
-  const fields = new Set(['content', 'repliedTo', 'commentId']);
+  const fields = new Set(['path', 'content', 'repliedTo', 'commentId']);
   const userId = (await auth())?.user?.id;
 
-  if (!userId) return authenticationErrorResponse(fields);
+  if (!userId) redirect('/login');
 
   if (!feedbackReply.success) return invalidFieldsErrorResponse(formData, feedbackReply.error, fields);
 
@@ -29,6 +26,7 @@ export const createFeedbackReplyAction = async (
     const feedbackComment = await getFeedbackComment(feedbackReply.data.commentId);
     await createFeedbackReply(userId, { ...feedbackReply.data, commentId: feedbackComment.id });
 
+    revalidatePath(feedbackReply.data.path);
     return resolveHTTPResponse('OK', 'reply successfully created', fields);
   } catch (error) {
     assertIsError(error);
