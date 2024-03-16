332a1420f61a871c752e2418af68556af86e3f7b
Renan Gomes <renango2015@gmail.com>
2024-03-16 20:59:45 -0300

    refactor: improve formatCategoryName function

--- src/utils/formatCategoryName.ts
+++ src/utils/formatCategoryName.ts
@@ -1,3 +1,16 @@
-export const formatCategoryName = (category: string) => {
-  return category !== 'UX' && category !== 'UI' ? category.toLowerCase() : category;
+import { Category } from '@/components/UpdateFeedback/UpdateFeedback';
+
+export const formatCategoryName = (category: Category) => {
+  switch (category) {
+    case 'BUG':
+      return 'Bug';
+    case 'ENHANCEMENT':
+      return 'Enhancement';
+    case 'FEATURE':
+      return 'Feature';
+    case 'UI':
+      return 'UI';
+    case 'UX':
+      return 'UX';
+  }
 };
