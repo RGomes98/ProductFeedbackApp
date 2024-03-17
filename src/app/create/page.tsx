59f05f365d2da0fc87bc821fc30946534f53059d
Renan Gomes <renango2015@gmail.com>
2024-03-16 21:03:45 -0300

    feat: add server-side redirect on create feedback page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,5 +1,10 @@
 import { CreateFeedback } from '@/components/CreateFeedback/CreateFeedback';
+import { redirect } from 'next/navigation';
+import { auth } from '@/auth';
 
-export default function Page() {
+export default async function Page() {
+  const userId = (await auth())?.user?.id;
+  if (!userId) redirect('/');
+
   return <CreateFeedback />;
 }
