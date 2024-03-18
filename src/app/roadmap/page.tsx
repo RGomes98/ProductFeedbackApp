390ec01534ba7981a91cb41f0a8c207856b678ef
Renan Gomes <renango2015@gmail.com>
2024-03-18 00:39:55 -0300

    fix: add standard value to feedback status filter

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -2,5 +2,5 @@ import { Status } from '@/components/UpdateFeedbackPage/UpdateFeedbackPage';
 import { RoadmapPage } from '@/components/RoadmapPage/RoadmapPage';
 
 export default async function Page({ searchParams: { status } }: { searchParams: { status: Status } }) {
-  return <RoadmapPage status={status} />;
+  return <RoadmapPage status={status || 'PLANNED'} />;
 }
